f78849d95da9c327ddce4a6367a55ad007870e9b
Saifur Rahman <msaifurrahman01@gmail.com>
2021-02-02 10:55:40 +0600

    Truthy and Falsy

---
+++ True.False.js
@@ -0,0 +1,24 @@
+
+// False
+// 0
+// ""
+?
+// undefined
+// nullNan
+
+// True
+// "0"
+// " "
+// []
+// {}
+
+
+let Username = null;
+console.log(Username);
+
+if(Username.length){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
